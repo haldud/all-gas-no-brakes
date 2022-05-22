f90e550d62639554d76dca9fe11ac840d421deba
m031506 <dudakovic.halil@mayo.edu>
2022-05-21 22:21:43 -0500

    updated to not run on localhost

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const http = require('http');
 
-const port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -11,6 +8,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}/`);
 });
