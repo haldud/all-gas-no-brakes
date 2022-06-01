46daf106ed73853489133b70388c97ebff1b41ac
m031506 <dudakovic.halil@mayo.edu>
2022-06-01 00:58:43 -0500

    dashboard progress

--- public/js/cssr.js
+++ public/js/cssr.js
@@ -1,10 +1,48 @@
-function getAgeCounts() {
-    console.log("got to age counts");
-    d3.json("cssr/ageCounts").then(function(data) {
-        console.log(data);
-      })
-      .catch(function(error) {
-        console.log(error);
-        // Do some error handling.
-      });
+setNumberOfAccidents();
+setNumberOfInjuries();
+setNumberOfSeriousInjuries();
+setNumberOfFatalities();
+
+function setNumberOfAccidents() {
+  document.getElementById("number-of-accidents").innerHTML = "Loading...";
+  d3.json("cssr/numberOfAccidents").then(function(data) {
+      document.getElementById("number-of-accidents").innerHTML = parseInt(data["number_of_accidents"]).toLocaleString();
+    })
+    .catch(function(error) {
+      console.log(error);
+      // Do some error handling.
+    });
+}
+
+function setNumberOfInjuries() {
+  document.getElementById("number-of-injuries").innerHTML = "Loading...";
+  d3.json("cssr/numberOfInjuries").then(function(data) {
+      document.getElementById("number-of-injuries").innerHTML = parseInt(data["number_of_injuries"]).toLocaleString();
+    })
+    .catch(function(error) {
+      console.log(error);
+      // Do some error handling.
+    });
+}
+
+function setNumberOfSeriousInjuries() {
+  document.getElementById("number-of-serious-injuries").innerHTML = "Loading...";
+  d3.json("cssr/numberOfSeriousInjuries").then(function(data) {
+      document.getElementById("number-of-serious-injuries").innerHTML = parseInt(data["number_of_serious_injuries"]).toLocaleString();
+    })
+    .catch(function(error) {
+      console.log(error);
+      // Do some error handling.
+    });
+}
+
+function setNumberOfFatalities() {
+  document.getElementById("number-of-fatalities").innerHTML = "Loading...";
+  d3.json("cssr/numberOfFatalities").then(function(data) {
+      document.getElementById("number-of-fatalities").innerHTML = parseInt(data["number_of_fatalities"]).toLocaleString();
+    })
+    .catch(function(error) {
+      console.log(error);
+      // Do some error handling.
+    });
 }
--- routes/cssr.js
+++ routes/cssr.js
@@ -4,6 +4,73 @@ var router = express.Router();
 const pgp = require('pg-promise')(/* options */)
 const db = pgp(process.env.CSSR_DB)
 
+/* GET number of accidents. */
+router.get('/numberOfAccidents', function(req, res, next) {
+  db.one('SELECT COUNT(*) AS "number_of_accidents" FROM public.accident')
+  .then((data) => {
+    console.log('DATA:', data)
+    res.json(data);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+    res.status(500);
+  })
+});
+
+/* GET number of any injuries. */
+router.get('/numberOfInjuries', function(req, res, next) {
+  db.one(`
+    SELECT COUNT(*) AS "number_of_injuries"
+    FROM public.person per
+    WHERE per.injsev_imname = 'Suspected Serious Injury (A)'
+    OR per.injsev_imname = 'Possible Injury (C)'
+    OR per.injsev_imname = 'Suspected Minor Injury (B)'
+    OR per.injsev_imname = 'Fatal Injury (K)'
+    OR per.injsev_imname = 'Injured, Severity Unknown'
+    OR per.injsev_imname = 'Died Prior to Crash*';`)
+  .then((data) => {
+    console.log('DATA:', data)
+    res.json(data);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+    res.status(500);
+  })
+});
+
+/* GET number of serious injuries. */
+router.get('/numberOfSeriousInjuries', function(req, res, next) {
+  db.one(`
+    SELECT COUNT(*) AS "number_of_serious_injuries"
+    FROM public.person per
+    WHERE per.injsev_imname = 'Suspected Serious Injury (A)';`)
+  .then((data) => {
+    console.log('DATA:', data)
+    res.json(data);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+    res.status(500);
+  })
+});
+
+/* GET number of fatalities. */
+router.get('/numberOfFatalities', function(req, res, next) {
+  db.one(`
+    SELECT COUNT(*) AS "number_of_fatalities"
+    FROM public.person per
+    WHERE per.injsev_imname = 'Fatal Injury (K)'
+    OR per.injsev_imname = 'Died Prior to Crash*';`)
+  .then((data) => {
+    console.log('DATA:', data)
+    res.json(data);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+    res.status(500);
+  })
+});
+
 /* GET age counts. */
 router.get('/ageCounts', function(req, res, next) {
   db.many('SELECT * FROM public.chart_age_counts')
